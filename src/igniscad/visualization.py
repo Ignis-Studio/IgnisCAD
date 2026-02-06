e8e67255976473c9e91a87ab6ab7de7f0b6b2f9f
Ext0gu1sher <danielhuang070213@qq.com>
2026-02-07 00:14:00 +0800

    feat: ablility to specify visualization mode in *show()*

--- src/igniscad/visualization.py
+++ src/igniscad/visualization.py
@@ -3,34 +3,37 @@ Visualize models via YACV or exporting them to disk.
 """
 
 import webbrowser
+from typing import Literal
+
 import build123d as bd
 
+from igniscad import Model
 
-def show(model):
+def _show_yacv_model(model: Model) -> bool:
     """
     Try connecting Yet Another CAD Viewer (within the browser)，
-    If failed: fallback to save and open the *.stl file.
     """
-    label = model.name or "Model"
-    print(f"👀 Processing: {label}")
-
-    # Try connecting Yet Another CAD Viewer (YACV)
     try:
         from yacv_server import show as yacv_show
         target_obj = model
-        yacv_show(target_obj, names=[label])
+        yacv_show(target_obj, names=[model.name])
 
         url = "http://localhost:32323"
         print(f"✅ Sent to YACV (Check your browser, at {url})")
         webbrowser.open(url)
         input("✅ Press Enter to exit...")
-        return
+        return True
     except Exception as e:
         print(f"⚠️ Failed to connect to YACV: {e}")
+        return False
 
-    # If failed: fallback to save and open the *.stl file.
-    print("⚠️ Viewer not available. Exporting to disk...")
-    filename = f"{label}.stl"
+
+
+def _export_stl_file(model):
+    """
+    Try exporting the specified model to a *.stl file.
+    """
+    filename = f"{model.name}.stl"
 
     try:
         bd.export_stl(model, filename)
@@ -47,3 +50,28 @@ def show(model):
         os.startfile(abs_path)
     except:
         pass
+
+def show(model: Model, mode: Literal['fallback', 'yacv', 'export'] = "fallback") -> None:
+    """
+    Visualize the specified model.
+    Args:
+        model (Model): The model to visualize
+        mode (Literal['fallback', 'yacv', 'export'] = "fallback"): The method of visualization.
+            "Fallback": to export the file when YACV is unavailable.
+            "yacv": to visualize the model via Yet Another CAD Viewer.
+            "export": to export the model to a *.stl file.
+    """
+    print(f"👀 Processing: {model.name}")
+    match mode:
+        case "fallback":
+            if not _show_yacv_model(model):
+                print("⚠️ Viewer not available. Exporting to disk...")
+                _export_stl_file(model)
+        case "yacv":
+            _show_yacv_model(model)
+        case "export":
+            _export_stl_file(model)
+        case _:
+            raise ValueError("❌ Invalid visualization method.\nCan be: \n\tfallback, yacv, export.")
+
+
